050edbb7e76d1d6c742c40a8c4acb2e27e6a0f49
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 13:57:17 +0200

    Correction avec la classe

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <title>Exercice 1</title>
+    <title>Exercice 2</title>
   </head>
   <body>
-    <!--Création d'un formulaire simple avec post en méthode-->
-<form class="formDeQualité" action="user.php" method="post">
-  <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" />
-  <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" />
-  <input type="submit" name="submit" />
-</form>
+    <!--Création d'un formulaire simple avec la méthode POST-->
+    <form action="user.php" method="POST">
+      <!--Champs nom-->
+      <label for="lastName">Votre nom: </label><input type="text" name="lastName" id="lastName" placeholder="nom" />
+      <!--Champs prénom-->
+      <label for="firstName">Votre prénom: </label><input type="text" name="firstName" id="firstName" placeholder="prénom" />
+      <input type="submit" value="Page Suivante" />
+      <!--Cliquer sur "Page Suivante" va garder en mémoire les données inscrite dans la superglobale $_POST['']-->
+    </form>
   </body>
 </html>
